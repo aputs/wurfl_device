8fe6099cb81d280c3a039ea9ae57baaaae3e6452
Allan Ralph Hutalla <ahutalla@gmail.com>
2011-11-04 15:20:03 +0800

    web service interface fix for method name changes

--- README.md
+++ README.md
@@ -1,22 +1,46 @@
-# WurflDevice
+WurflDevice
+===========
 Ruby client library for mobile handset detection
 
 
-requirements
+Requirements
 ------------
-  * [redis server](http://redis.io/)
+* [redis server](http://redis.io/)
 
 
-usage
+Installation
+------------
+install using rubygems
+
+    gem install wurfl_device
+
+or add to your Gemfile:
+
+    gem wurfl_device
+
+and install it via Bundler:
+
+    $ bundle
+
+
+Usage
 -----
 
     require 'wurfl_device'
 
+get capabilities hash from user agent
+
     user_agent = 'Mozilla/5.0 (SymbianOS/9.2; U; Series60/3.1 NokiaN95_8GB/20.0.016; Profile/MIDP-2.0 Configuration/CLDC-1.1 ) AppleWebKit/413 (KHTML, like Gecko) Safari/413'
+    capabilities = WurflDevice.capabilities_from_user_agent(user_agent)
 
-    device = WurflDevice.get_device_from_ua(user_agent)
+get capabilities hash from device id
 
-    # see device capabilities list for names
-    puts device.id
+    device_id = 'generic'
+    capabilities = WurflDevice.capabilities_from_id(device_id)
 
+get capability from user agent
+
+    user_agent = 'generic'
+    capability_name = 'id'
+    capability = WurflDevice.capabilities(capability_name, user_agent)
 
--- lib/wurfl_device/web_service.rb
+++ lib/wurfl_device/web_service.rb
@@ -6,57 +6,22 @@ require 'sinatra/base'
 module WurflDevice
   class WebService < Sinatra::Base
     get '/' do
-      user_agent = request.env['HTTP_USER_AGENT'] || '-'
-      device = WurflDevice.get_device_from_ua(user_agent)
-      return device.capabilities.to_yaml unless device.nil?
-      {}.to_yaml
-    end
-
-    get '/:capa' do
-      user_agent = request.env['HTTP_USER_AGENT'] || '-'
-      device = WurflDevice.get_device_from_ua(user_agent)
-      return device.send(params[:capa]).to_yaml unless device.nil?
-      {}.to_yaml
-    end
-
-    get '/device/:id' do
-      device = WurflDevice.get_device_from_id(params[:id])
-      capabilities = {}
-      capabilities = device.capabilities unless device.nil?
+      user_agent = request.env['HTTP_USER_AGENT']
+      user_agent = WurflDevice::Settings::GENERIC if user_agent.nil? || user_agent.empty?
+      capabilities = WurflDevice.capabilities_from_user_agent(user_agent)
       capabilities.to_yaml
     end
 
-    get '/device/:id/:capa' do
-      device = WurflDevice.get_device_from_id(params[:id])
-      capability = {}
-      capability = device.send(params[:capa]) unless device.nil?
+    get '/capability/:id' do
+      user_agent = request.env['HTTP_USER_AGENT']
+      user_agent = WurflDevice::Settings::GENERIC if user_agent.nil? || user_agent.empty?
+      capability = WurflDevice.capability_from_user_agent(params[:id], user_agent)
       capability.to_yaml
     end
 
-    get '/actual_device/:id' do
-      capabilities = WurflDevice.get_actual_device(params[:id])
+    get '/device/:id' do
+      capabilities = WurflDevice.capabilities_from_id(params[:id])
       capabilities.to_yaml
-    end
-
-    get '/actual_device/:id/:capa' do
-      capabilities = WurflDevice.get_actual_device(params[:id])
-      get_capa(params[:capa], capabilities).to_yaml
-    end
-
-  protected
-    def get_capa(capa, capabilities)
-      capability = nil
-      if capabilities.has_key?(capa)
-        capability = capabilities.send(capa)
-      else
-        capabilities.each_pair do |key, value|
-          if value.is_a?(Hash)
-            capability = value.send(capa)
-            break
-          end
-        end
-      end
-      return capability
     end
   end
 end
