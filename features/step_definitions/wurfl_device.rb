a097abaaac934c4e292581da168d000fe3414107
Allan Ralph Hutalla <ahutalla@gmail.com>
2012-03-29 03:34:30 +0800

    forgot to open the url before gunzipping

--- features/step_definitions/wurfl_device.rb
+++ features/step_definitions/wurfl_device.rb
@@ -12,7 +12,7 @@ end
 When /^I download the xml file saving it as "([^"]*)"$/ do |xml_file|
   @saved_xml_file = xml_file
   next if File.exists?(xml_file)
-  File.open(xml_file, 'w') { |f| f.write(Zlib::GzipReader.new(gzipped_url_to_download).read) }
+  File.open(xml_file, 'w') { |f| f.write(Zlib::GzipReader.new(open(@gzipped_url_to_download)).read) }
 end
 
 Then /^I should see the xml file$/ do
