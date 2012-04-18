ab077c2243fca8fe945a8f906f4b82bd244fe178
Allan Ralph Hutalla <ahutalla@gmail.com>
2012-04-18 20:27:33 +0800

    using sinatra

--- lib/wurfl_device/web_service.rb
+++ lib/wurfl_device/web_service.rb
@@ -1,10 +1,10 @@
-require 'json'
+require 'sinatra'
 
 module WurflDevice
-  class WebService
-    def call(env)
-      handset = WurflDevice.handset_from_user_agent(env['HTTP_USER_AGENT'] || '-')
-      [200, {"Content-Type" => "application/json"}, [handset.full_capabilities.to_json]]
+  class WebService < Sinatra::Application
+    def handset_from_user_agent
+      cache_control :public, :must_revalidate, :max_age => 60
+      [200, {"Content-Type" => "application/json"}, [WurflDevice.handset_from_user_agent(request.user_agent).full_capabilities.to_json]]
     end
   end
 end
--- wurfl_device.gemspec
+++ wurfl_device.gemspec
@@ -21,6 +21,8 @@ Gem::Specification.new do |s|
   s.add_dependency 'text'
   s.add_dependency 'thor'
   s.add_dependency 'puma'
+  s.add_dependency 'rack-cache'
+  s.add_dependency 'sinatra'
 
   s.add_development_dependency 'bundler'
   s.add_development_dependency 'rake'
