22406357904ea166a0276ff420d702aae1ad421c
Allan Ralph Hutalla <ahutalla@gmail.com>
2012-04-19 13:27:41 +0800

    modularized the rack/sinatra

--- lib/wurfl_device/web_service.rb
+++ lib/wurfl_device/web_service.rb
@@ -1,10 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
 
 module WurflDevice
-  class WebService < Sinatra::Application
+  class WebService < Sinatra::Base
+
     def handset_from_user_agent
-      cache_control :public, :must_revalidate, :max_age => 60
       [200, {"Content-Type" => "application/json"}, [WurflDevice.handset_from_user_agent(request.user_agent).full_capabilities.to_json]]
+    end
+
+    get '/' do
+      handset_from_user_agent
+    end
+
+    error 404 do
+      @app.call(env)
     end
   end
 end
