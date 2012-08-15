58c75854abf6f08367b5e29f6a7f6d59aca098aa
Andre Medeiros <me@andremedeiros.info>
2012-08-15 16:57:32 +0200

    Point rake tasks to the correct file

--- lib/wurfl_device/railtie.rb
+++ lib/wurfl_device/railtie.rb
@@ -3,7 +3,7 @@
 module WurflDevice
   class Railtie < ::Rails::Railtie
     rake_tasks do
-      load File.join(File.expand_path('../..', File.dirname(__FILE__)), 'lib', 'tasks', 'wurfl.rake')
+      load File.join(File.expand_path('../..', File.dirname(__FILE__)), 'rakelib', 'wurfl_device.rake')
     end
   end
 end
